69f880ccb87bdcd6c07d328f3742016b13237599
xr <1653931460@qq.com>
2019-04-27 20:22:03 +0800

    第一次修改

--- src/main/java/com/atguigu/khbd/Lhc.java
+++ src/main/java/com/atguigu/khbd/Lhc.java
@@ -3,5 +3,8 @@ package com.atguigu.khbd;
 public class Lhc {
     public static void main(String[] args) {
         System.out.println("11111111111111");
+        System.out.println("22222222222222");
+        System.out.println("33333333333333");
+        System.out.println("44444444444444");
     }
 }
